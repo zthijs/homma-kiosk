89976423cad57e2eecb85debab870b015c7eb87c
zthijs <zthijs@outlook.com>
2022-07-15 10:05:46 +0200

    gaaf

--- install.sh
+++ install.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Je moet dit script als root uitvoeren"
+  exit
+fi
+
+sudo apt-get update
+sudo apt-get upgrade
+
+
+cd ~
+
+wget http://steinerdatenbank.de/software/kweb-1.7.9.8.tar.gz
+tar -xzf kweb-1.7.9.8.tar.gz
+cd kweb-1.7.9.8
+./debinstall
+
+cd ~
+
+wget http://steinerdatenbank.de/software/kweb_upgrade_buster_20190823.tar.gz
+tar -xzf kweb_upgrade_buster_20190823.tar.gz
+cd kweb_upgrade_buster_20190823
+./install
+
+echo "Kweb is geinstalleerd"
+read url
+
+printf "xset s off\nxset s noblank\nxset -dpms\nsetxkbmap -option terminate:ctrl_alt_bksp\nkweb -KHCUAJ+-zbhrqfpoklgtje '$url'" >> /etc/xdg/openbox/autostart
+
+su homma
+cd ~
+
+echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor" >> .bash_profile
