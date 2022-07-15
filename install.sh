fb2d0c8d2bda9d63c61c4e1cac304db6459fd9f9
zthijs <zthijs@outlook.com>
2022-07-15 10:54:39 +0200

    final

--- install.sh
+++ install.sh
@@ -5,6 +5,26 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+sudo apt-get update
+sudo apt-get upgrade
+
+
+cd ~
+
+wget http://steinerdatenbank.de/software/kweb-1.7.9.8.tar.gz
+tar -xzf kweb-1.7.9.8.tar.gz
+cd kweb-1.7.9.8
+./debinstall
+
+cd ~
+
+wget http://steinerdatenbank.de/software/kweb_upgrade_buster_20190823.tar.gz
+tar -xzf kweb_upgrade_buster_20190823.tar.gz
+cd kweb_upgrade_buster_20190823
+./install
+
+echo "Vul de URL voor het scherm in : "
+read url
 
 printf "xset s off\nxset s noblank\nxset -dpms\nsetxkbmap -option terminate:ctrl_alt_bksp\nkweb -KHCUAJ+-zbhrqfpoklgtje '$url'" > /etc/xdg/openbox/autostart
 
